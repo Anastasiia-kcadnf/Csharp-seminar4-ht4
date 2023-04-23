d75ffd6ec328b319bf014eb9836b99beadb512c1
Nastja <nastasjaandre@gmail.com>
2023-04-23 09:57:50 +0300

    added task3

---
+++ task3/Program.cs
@@ -0,0 +1,31 @@
+﻿// Задача 3 Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран.
+// 1, 2, 5, 7, 19 -> [1, 2, 5, 7, 19]
+// 6, 1, 33 -> [6, 1, 33]
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int[] CreateArray(int size)
+{
+    int[] array = new int[8];
+    for(int i = 0; i < 8; i++)
+    {
+        Console.Write($"Enter array[{i}]: ");
+        array[i] = Convert.ToInt32(Console.ReadLine());
+    }
+    return array;
+}
+
+void PrintArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+    Console.Write("\n");
+}
+
+int number = ReadInt("Input a number");
+int[] CreateArray;
+PrintArray(array);
