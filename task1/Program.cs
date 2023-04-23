f5aa8130a88be250ae92927e90aca1d86a1eaa52
Nastja <nastasjaandre@gmail.com>
2023-04-23 09:44:20 +0300

    added task1

---
+++ task1/Program.cs
@@ -0,0 +1,23 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int ReadInt(string message)
+{
+    System.Console.Write($"{message} > ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int Power(int baseNumberA, int pow)
+{
+    int result = 1;
+    for(int i = 1; i <= pow; i++)
+        result *= baseNumberA;
+    return result;
+    
+}
+
+int baseNumA = ReadInt("Input number A (base)");
+int powNumB = ReadInt("Input number B (power)");
+int result = Power(baseNumA, powNumB);
+System.Console.WriteLine($"{baseNumA} raised to the power of {powNumB} is {result}.");
